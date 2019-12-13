b603154d2c62146c5f7c721b3ca0b21e0748bc8f
Tom Greenwood <31808084+tgreenwood93@users.noreply.github.com>
2019-12-13 16:28:46 -0700

    Add files via upload

---
+++ build/esp32/clk.d
@@ -0,0 +1,230 @@
+clk.o: /home/tom/esp/esp-idf/components/esp32/clk.c \
+ /home/tom/esp/esp-idf/components/newlib/include/stdint.h \
+ /home/tom/esp/esp-idf/components/newlib/include/machine/_default_types.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/features.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/_intsup.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/cdefs.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/time.h \
+ /home/tom/esp/esp-idf/components/newlib/include/_ansi.h \
+ /home/tom/esp/esp-idf/components/newlib/include/newlib.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/config.h \
+ /home/tom/esp/esp-idf/components/newlib/include/machine/ieeefp.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/config/core-isa.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/types.h \
+ /home/tom/esp/esp-idf/components/newlib/include/machine/_types.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/_types.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/lock.h \
+ /home/tom/esp/esp-idf/components/newlib/include/machine/types.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/sched.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/param.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/syslimits.h \
+ /home/tom/esp/esp-idf/components/newlib/include/machine/endian.h \
+ /home/tom/esp/esp-idf/components/newlib/include/machine/param.h \
+ /home/tom/esp/testDev/build/include/sdkconfig.h \
+ /home/tom/esp/esp-idf/components/esp32/include/esp_attr.h \
+ /home/tom/esp/esp-idf/components/log/include/esp_log.h \
+ /home/tom/esp/esp-idf/components/esp32/include/rom/ets_sys.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/soc.h \
+ /home/tom/esp/esp-idf/components/esp32/include/esp_assert.h \
+ /home/tom/esp/esp-idf/components/newlib/platform_include/assert.h \
+ /home/tom/esp/esp-idf/components/newlib/include/stdlib.h \
+ /home/tom/esp/esp-idf/components/newlib/include/_ansi.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/reent.h \
+ /home/tom/esp/esp-idf/components/newlib/include/machine/stdlib.h \
+ /home/tom/esp/esp-idf/components/newlib/include/alloca.h \
+ /home/tom/esp/esp-idf/components/newlib/include/assert.h \
+ /home/tom/esp/esp-idf/components/log/include/esp_log_internal.h \
+ /home/tom/esp/esp-idf/components/esp32/include/esp_clk.h \
+ /home/tom/esp/esp-idf/components/esp32/esp_clk_internal.h \
+ /home/tom/esp/esp-idf/components/esp32/include/rom/uart.h \
+ /home/tom/esp/esp-idf/components/esp32/include/esp_types.h \
+ /home/tom/esp/esp-idf/components/esp32/include/rom/ets_sys.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/uart_reg.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/soc.h \
+ /home/tom/esp/esp-idf/components/esp32/include/rom/rtc.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/rtc.h \
+ /home/tom/esp/esp-idf/components/soc/include/soc/rtc_wdt.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/rtc_cntl_reg.h \
+ /home/tom/esp/esp-idf/components/esp32/include/esp_err.h \
+ /home/tom/esp/esp-idf/components/newlib/include/stdio.h \
+ /home/tom/esp/esp-idf/components/newlib/include/sys/stdio.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/i2s_reg.h \
+ /home/tom/esp/esp-idf/components/driver/include/driver/periph_ctrl.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/dport_reg.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/dport_access.h \
+ /home/tom/esp/esp-idf/components/esp32/include/esp_dport_access.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/uart_reg.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/xtruntime.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/config/core.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/hal.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/xtensa-versions.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/config/core-matmap.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/config/tie.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/config/specreg.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/corebits.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/xtruntime-core-state.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/xtruntime-frames.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/periph_defs.h \
+ /home/tom/esp/esp-idf/components/esp32/include/xtensa/core-macros.h \
+ /home/tom/esp/esp-idf/components/bootloader_support/include/bootloader_clock.h \
+ /home/tom/esp/esp-idf/components/driver/include/driver/spi_common.h \
+ /home/tom/esp/esp-idf/components/esp32/include/rom/lldesc.h \
+ /home/tom/esp/esp-idf/components/esp32/include/rom/queue.h \
+ /home/tom/esp/esp-idf/components/soc/include/soc/spi_periph.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/spi_pins.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/spi_reg.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/spi_struct.h \
+ /home/tom/esp/esp-idf/components/soc/esp32/include/soc/gpio_sig_map.h
+
+/home/tom/esp/esp-idf/components/newlib/include/stdint.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/machine/_default_types.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/features.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/_intsup.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/cdefs.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/time.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/_ansi.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/newlib.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/config.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/machine/ieeefp.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/config/core-isa.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/types.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/machine/_types.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/_types.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/lock.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/machine/types.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/sched.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/param.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/syslimits.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/machine/endian.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/machine/param.h:
+
+/home/tom/esp/testDev/build/include/sdkconfig.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/esp_attr.h:
+
+/home/tom/esp/esp-idf/components/log/include/esp_log.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/rom/ets_sys.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/soc.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/esp_assert.h:
+
+/home/tom/esp/esp-idf/components/newlib/platform_include/assert.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/stdlib.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/_ansi.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/reent.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/machine/stdlib.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/alloca.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/assert.h:
+
+/home/tom/esp/esp-idf/components/log/include/esp_log_internal.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/esp_clk.h:
+
+/home/tom/esp/esp-idf/components/esp32/esp_clk_internal.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/rom/uart.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/esp_types.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/rom/ets_sys.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/uart_reg.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/soc.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/rom/rtc.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/rtc.h:
+
+/home/tom/esp/esp-idf/components/soc/include/soc/rtc_wdt.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/rtc_cntl_reg.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/esp_err.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/stdio.h:
+
+/home/tom/esp/esp-idf/components/newlib/include/sys/stdio.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/i2s_reg.h:
+
+/home/tom/esp/esp-idf/components/driver/include/driver/periph_ctrl.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/dport_reg.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/dport_access.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/esp_dport_access.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/uart_reg.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/xtruntime.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/config/core.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/hal.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/xtensa-versions.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/config/core-matmap.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/config/tie.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/config/specreg.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/corebits.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/xtruntime-core-state.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/xtruntime-frames.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/periph_defs.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/xtensa/core-macros.h:
+
+/home/tom/esp/esp-idf/components/bootloader_support/include/bootloader_clock.h:
+
+/home/tom/esp/esp-idf/components/driver/include/driver/spi_common.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/rom/lldesc.h:
+
+/home/tom/esp/esp-idf/components/esp32/include/rom/queue.h:
+
+/home/tom/esp/esp-idf/components/soc/include/soc/spi_periph.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/spi_pins.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/spi_reg.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/spi_struct.h:
+
+/home/tom/esp/esp-idf/components/soc/esp32/include/soc/gpio_sig_map.h:
+/home/tom/esp/esp-idf/components/esp32/./clk.c:
